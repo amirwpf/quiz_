68a5fee5271208dc191388498ff75a5a21be6f6b
amirwpf <hh1900dppp@gmail.com>
2023-07-11 20:23:28 +0430

    frst commit

---
+++ quiz_/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using quiz_.Models;
+using System.Diagnostics;
+
+namespace quiz_.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    class HomeController : Controller
+    {
+        [HttpDelete]
+        public IActionResult ExampleAction(string actionName)
+        {
+            return null;
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpDelete]
+        public IActionResult AnotherAction(string actionName)
+        {
+            return null;
+        }
+    }
+}
